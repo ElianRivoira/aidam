bf72aa5fe63ac6455fb6924b89fa07b96df0cf5d
ElianRivoira <eli_rivoira@hotmail.com>
2023-03-16 11:22:29 -0300

    initial setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# aidam
-aidam app
---
+++ aidam-app/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const prod = process.env.NODE_ENV === 'production'
+
+const withPWA = require("next-pwa")({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: prod ? false : true
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
