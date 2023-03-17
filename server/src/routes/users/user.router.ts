e73b81e8f7dc411342b1701b7e920d210258b8b9
ElianRivoira <eli_rivoira@hotmail.com>
2023-03-17 16:46:45 -0300

    fixed errors and cookies

--- server/src/routes/users/user.router.ts
+++ server/src/routes/users/user.router.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import userController from './user.controller';
-import { validateSignUp } from '../../middlewares/userValidators';
-import { validateLoggedUser } from '../../middlewares/auth';
+import { validateLogin, validateSignUp } from '../../middlewares/userValidators';
+import { validateLoggedUser } from '../../middlewares/userValidators';
 
 const router = express.Router();
 
 router.post('/', validateSignUp, userController.httpSignUp);
-router.post('/login', userController.httpUserLogin);
-router.get('/me', [validateLoggedUser], userController.httpGetUser);
+router.post('/login', validateLogin, userController.httpUserLogin);
+router.get('/me', validateLoggedUser, userController.httpGetUser);
 
 export default router;
