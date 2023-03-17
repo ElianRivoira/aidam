8fc3fd001e3acd28e391820858bfe9da37cd8c08
ElianRivoira <eli_rivoira@hotmail.com>
2023-03-17 12:08:57 -0300

    post observation endpoint

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import userRouter from './users/user.router'
 
+import userRouter from './users/user.router';
+import observationRouter from './observation/observation.router';
 
 const api = express.Router();
 
 api.use('/users', userRouter);
+api.use('/observations', observationRouter);
 
 export default api;
