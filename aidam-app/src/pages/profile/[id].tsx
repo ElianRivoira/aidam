b4681c062379f11b504d69ce37c8ca4679933981
RoberFonseca <roberfonseca944@gmail.com>
2023-04-26 11:10:58 -0300

    added links to listed patients

--- aidam-app/src/pages/profile/[id].tsx
+++ aidam-app/src/pages/profile/[id].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import useMediaQuery from '@/hooks/useMediaQuery';
@@ -17,6 +17,8 @@ import { hasCookie } from 'cookies-next';
 import NavbarDesktop from '@/components/navbar/NavbarDesktop';
 import { useRouter } from 'next/router';
 import { NextPageContext } from 'next';
+import ArrowBack from '@/components/ArrowBack';
+import Link from 'next/link';
 
 const Profile = ({ query }: MyPageProps) => {
   const {
@@ -26,10 +28,38 @@ const Profile = ({ query }: MyPageProps) => {
     error,
     isSuccess,
   } = useQuery({
-    queryKey: ['user'],
+    queryKey: ['user', query.id],
     enabled: hasCookie('session'),
     queryFn: () => findUserById(query.id),
   });
+
+  const [filteredPatients, setFilteredPatients] = useState<Patient[]>();
+  const [browserPatients, setbrowserPatients] = useState('');
+
+  const filter = (patientsArr: Patient[], letters: string) => {
+    if (letters === '') {
+      setFilteredPatients(patientsArr);
+      return;
+    }
+    const filteredPatients = patientsArr.filter(
+      (patient: Patient) =>
+        patient.firstName.includes(letters) ||
+        patient.lastName.includes(letters)
+    );
+    setFilteredPatients(filteredPatients);
+  };
+
+  useEffect(() => {
+    if (user?.patientsId) {
+      setFilteredPatients(user?.patientsId);
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (user?.patientsId) {
+      filter(user.patientsId, browserPatients);
+    }
+  }, [browserPatients]);
 
   return (
     <>
@@ -37,47 +67,131 @@ const Profile = ({ query }: MyPageProps) => {
         <title>AIDAM - Perfil</title>
       </Head>
       <div className='h-screen flex flex-col items-center'>
-        {useMediaQuery(1024) ? <Navbar /> : <NavbarDesktop />}
-        <div className='px-3.5 w-full max-w-md md:border md:shadow-xg md:rounded-3xl md:mt-5'>
-          <div className='flex justify-end mt-3'>
-            <button className='font-light text-xm flex flex-col items-center'>
-              <Image
-                src={profileEdit}
-                alt='editar perfil'
-                width={25}
-                height={25}
-                className='-mb-3'
-              />
-              <br />
-              Editar perfil
-            </button>
-          </div>
-          <div className='flex flex-col items-center'>
-            <Image src={profileImage} alt='imagen' className='' />
-            <p className='font-semibold text-lb'>
-              {user?.firstName.toUpperCase()} {user?.lastName.toUpperCase()}
-            </p>
-          </div>
-          <div className='mt-12 px-2.5'>
-            <h1 className='font-semibold text-lb mb-5.5'>DATOS PERSONALES</h1>
-            <Data
-              icon={professionLogo}
-              title='Profesión'
-              info={user?.profession}
-            />
-            <Data icon={licenseIcon} title='Matrícula' info={user?.license} />
-          </div>
-          <hr className='w-full border-black03' />
-          <div className='mt-8 px-2.5'>
-            <h1 className='font-semibold text-lb mb-5.5'>CONTACTO</h1>
-            <Data
-              icon={emailIcon}
-              title='Correo electrónico'
-              info={user?.email}
-            />
-            <Data icon={phoneIcon} title='Teléfono' info={user?.phone} />
-          </div>
-        </div>
+        {useMediaQuery(1024) ? (
+          <>
+            <Navbar />
+            <div className='px-3.5 w-full max-w-md md:border md:shadow-xg md:rounded-3xl md:mt-5'>
+              <div className='flex justify-end mt-3'>
+                <button className='font-light text-xm flex flex-col items-center'>
+                  <Image
+                    src={profileEdit}
+                    alt='editar perfil'
+                    width={25}
+                    height={25}
+                    className='-mb-3'
+                  />
+                  <br />
+                  Editar perfil
+                </button>
+              </div>
+              <div className='flex flex-col items-center'>
+                <Image src={profileImage} alt='imagen' className='' />
+                <p className='font-semibold text-lb'>
+                  {user?.firstName.toUpperCase()} {user?.lastName.toUpperCase()}
+                </p>
+              </div>
+              <div className='mt-12 px-2.5'>
+                <h1 className='font-semibold text-lb mb-5.5'>
+                  DATOS PERSONALES
+                </h1>
+                <Data
+                  icon={professionLogo}
+                  title='Profesión'
+                  info={user?.profession}
+                />
+                <Data
+                  icon={licenseIcon}
+                  title='Matrícula'
+                  info={user?.license}
+                />
+              </div>
+              <hr className='w-full border-black03' />
+              <div className='mt-8 px-2.5'>
+                <h1 className='font-semibold text-lb mb-5.5'>CONTACTO</h1>
+                <Data
+                  icon={emailIcon}
+                  title='Correo electrónico'
+                  info={user?.email}
+                />
+                <Data icon={phoneIcon} title='Teléfono' info={user?.phone} />
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <NavbarDesktop />
+            <div className='w-full px-12'>
+              <div className='flex mt-9 justify-between'>
+                <div>
+                  <ArrowBack />
+                </div>
+                <div>
+                  <button className='text-lb font-semibold text-white px-4 py-2.5 rounded-md bg-[#B81212CC] hover:bg-[#e26f6fcc] transition-colors'>
+                    Dar de baja
+                  </button>
+                </div>
+              </div>
+              <div className='flex flex-col items-center'>
+                <Image src={profileImage} alt='perfil' className='w-[90px]' />
+                <p className='font-semibold text-2xl'>
+                  {user?.firstName} {user?.lastName}
+                </p>
+              </div>
+              <div className='flex mt-14'>
+                <div className='flex flex-col font-semibold text-xl pl-20 w-1/3'>
+                  <h1 className='mb-10'>DATOS PERSONALES</h1>
+                  <div>
+                    <Data
+                      icon={professionLogo}
+                      title='Profesión'
+                      info={user?.profession}
+                    />
+                    <Data
+                      icon={licenseIcon}
+                      title='Matrícula'
+                      info={user?.license}
+                    />
+                  </div>
+                </div>
+                <div className='flex flex-col font-semibold border-x-2 text-xl w-1/3 items-center'>
+                  <h1 className='mb-10'>PACIENTES</h1>
+                  <div className='self-start px-12 flex-1 w-full'>
+                    <input
+                      placeholder='Buscar paciente'
+                      className='max-w-[250px] outline-none border rounded-md px-2 hover:border-aidam focus:border-aidam mb-4'
+                      onChange={(e) => setbrowserPatients(e.target.value)}
+                      value={browserPatients}
+                    />
+                    <div className='h-48 overflow-y-auto'>
+                      {filteredPatients?.map((patient, index) => (
+                        <li key={index}>
+                          <Link href={`/patients/${patient._id}/profile`}>
+                            {patient.firstName} {patient.lastName}
+                          </Link>
+                        </li>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className='flex flex-col font-semibold text-xl pl-20 w-1/3'>
+                  <h1 className='mb-10'>CONTACTO</h1>
+                  <div>
+                    <Data
+                      icon={emailIcon}
+                      title='Correo electrónico'
+                      info={user?.email}
+                    />
+                    <Data
+                      icon={phoneIcon}
+                      title='Teléfono'
+                      info={user?.phone}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
