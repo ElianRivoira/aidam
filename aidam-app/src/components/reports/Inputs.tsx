3bcd954496a0ac4a5aa0a08816ec0e02e0ed74ab
ElianRivoira <eli_rivoira@hotmail.com>
2023-05-17 13:44:57 -0300

    mobile medical report

--- aidam-app/src/components/reports/Inputs.tsx
+++ aidam-app/src/components/reports/Inputs.tsx
@@ -41,19 +41,20 @@ export const TextArea: React.FC<TextAreaProps> = ({
       <label
         htmlFor={name}
         className={`${labelclass} ${
-          labelclass?.includes('text') ? '' : 'text-ln'
+          labelclass?.includes('text') ? '' : 'text-ln lgMax:text-lb'
         } font-medium mb-2`}
       >
         {label}
       </label>
       <TextareaAutosize
+        required
         name={name}
         id={name}
         placeholder={placeholder}
         minRows={minRows ? minRows : 5}
         value={value}
         onChange={onChange}
-        className='rounded-md p-1.5 w-full shadow-xm resize-none outline-none border border-gray-100 hover:border-aidam80 transition-colors'
+        className='rounded-md lgMax:text-ss p-1.5 w-full shadow-xm resize-none outline-none border border-gray-100 hover:border-aidam80 transition-colors'
       />
     </div>
   );
@@ -72,18 +73,19 @@ export const DateInput: React.FC<InputProps> = ({
       <label
         htmlFor={name}
         className={`${labelclass} ${
-          labelclass?.includes('text') ? '' : 'text-ln'
+          labelclass?.includes('text') ? '' : 'text-ln lgMax:text-lb'
         } font-medium mr-2`}
       >
         {label}
       </label>
       <input
+        required
         type='date'
         name={name}
         id={name}
         value={value}
         onChange={onChange}
-        className='outline-none border border-background hover:border-aidam80 rounded-md transition-colors'
+        className='outline-none lgMax:text-lb border border-background hover:border-aidam80 rounded-md transition-colors'
       />
     </div>
   );
@@ -102,7 +104,7 @@ export const RadioInput: React.FC<InputProps> = ({
     <fieldset className={`flex gap-2 flex-wrap justify-between ${divclass}`}>
       <legend
         className={`${labelclass} ${
-          labelclass?.includes('text') ? '' : 'text-ln'
+          labelclass?.includes('text') ? '' : 'text-ln lgMax:text-lb'
         } font-medium mr-2 mb-2`}
       >
         {label}
@@ -111,6 +113,7 @@ export const RadioInput: React.FC<InputProps> = ({
         <div className='flex gap-2 items-center' key={itemValue}>
           <label htmlFor={itemValue}>{itemValue}</label>
           <input
+            required
             type='radio'
             id={itemValue}
             name={name}
@@ -143,6 +146,7 @@ export const TextInput: React.FC<InputProps> = ({
         {label}
       </label>
       <input
+        required
         type='text'
         name={name}
         id={name}
