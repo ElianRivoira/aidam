19b152b830a4569407d137e65d225bc8116b0a97
ElianRivoira <eli_rivoira@hotmail.com>
2023-06-02 16:33:17 -0300

    fix

--- aidam-app/next.config.js
+++ aidam-app/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
 
-const withPWA = require("next-pwa")({
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  // disable: true,
 });
 
 module.exports = withPWA({
